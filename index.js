23438dddbe3b9a98ca487b9bc6302337295c3b15
Aladdin <145193322+aladdin-y@users.noreply.github.com>
2024-01-27 21:46:25 +0200

    Update index.js
    Signed-off-by: Aladdin <145193322+aladdin-y@users.noreply.github.com>

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ $( function() {
 	function show(menu) {
 		$(".main_aladdin").hide("Flod");
 		$(".aladdin_AboutME_menu").hide("Flod");
+		$(".aladdin_work_menu").hide("Flod");
 
-console.log(menu)
 	$(`.${menu}`).show("Flod");
 
     }
@@ -62,11 +62,13 @@ $(".navbar-toggler").click(function(){
 
 $(document).ready(function() {
 	$(".aladdin_AboutME_menu").hide();
+	$(".aladdin_work_menu").hide();
 
 	$(".main_aladdin").show();
 
 
         $.get(`https://japi.rest/discord/v1/user/376752463971352577`).then((idk) => {
+			
 	 const data = idk.data;
 
 	 $( ".aladdin_profile_main" ).empty();
@@ -97,6 +99,48 @@ $(document).on('click', '.aladdin_AboutME', function() {
 	show("aladdin_AboutME_menu")
 })
 
+$(document).on('click', '.aladdin_work', function() {
+	$.get(`https://mini-furtive-donkey.glitch.me/aladdin_work`).then((idk) => {
+
+	  if (idk.success) {
+		$( ".my_work" ).empty();
+
+		const data = idk.data;
+  
+		// تحقق من وجود البيانات قبل الوصول إليها
+		if (data && data.length > 0) {
+		  // قم بفحص كل عنصر في المصفوفة
+
+		  data.forEach(item => {
+			let preview = "disabled";
+			let url = "#";
+			if(item.preview == true){
+				preview = "";
+				url = item.url;
+			}
+			const div = `
+			<div class="card  text-center text-bg-dark mb-3" style="width: 18rem; border: 2px solid var(--mainColor);">
+			<img src="${item.img}" class="card-img-top" alt="img">
+			<div class="card-body">
+			  <h5 class="card-title">${item.title}</h5>
+			  <p class="card-text">${item.description}</p>
+			  <a href="${url}" class="btn btn-primary ${preview}">Preview</a>
+			</div>
+		  </div> 
+			`
+			$( ".my_work" ).append(div );
+		});
+		} else {
+		  console.log('No data found');
+		}
+	  } else {
+		console.log('Request was not successful');
+	  }
+	});
+  
+	show("aladdin_work_menu");
+  });
+  
 // let time = 0;
 // let m = 0;
 
